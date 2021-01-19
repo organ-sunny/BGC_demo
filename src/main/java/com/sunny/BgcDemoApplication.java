e763e3b59e3151003f571ec97adbd31e98e53013
ZJ <1476253236@qq.com>
2021-01-19 16:43:07 +0800

    zhoujun commit

--- src/main/java/com/sunny/BgcDemoApplication.java
+++ src/main/java/com/sunny/BgcDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BgcDemoApplication {
 
     public static void main(String[] args) {
+		// zhoujun添加的注释
         SpringApplication.run(BgcDemoApplication.class, args);
     }
 
