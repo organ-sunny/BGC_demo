41c1c1b178e293381af541e5870bb8f5b2a7987c
yifulitesunny <1164806928@qq.com>
2021-01-21 16:01:49 +0800

    test3

--- src/main/java/com/sunny/BgcDemoApplication.java
+++ src/main/java/com/sunny/BgcDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BgcDemoApplication {
 
     public static void main(String[] args) {
-		// zhouJun添加的注释
         SpringApplication.run(BgcDemoApplication.class, args);
     }
 
