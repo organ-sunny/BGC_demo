6ef91d0c1deb3154db4d314abe5ef7018a2e6695
ZJ <1476253236@qq.com>
2021-01-30 13:25:53 +0800

    修复 修改用户名时右上角未变化

--- ui/src/component/home.vue
+++ ui/src/component/home.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="width: 100%;height: 100%;">
-        <div style="height: 5%;background-color: #2f89fc;display: flex;box-shadow: 0 1px 5px rgba(0,0,0,.2), 0 2px 2px rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12);z-index: 1;position: relative;">
+        <div style="height: 5%;background-color: #001529;display: flex;box-shadow: 0 1px 5px rgba(0,0,0,.2), 0 2px 2px rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12);z-index: 1;position: relative;">
             <div style="font-size: 18px;font-weight: bold;color: white;width: 10%;" class="center">
                 BGC_demo
             </div>
@@ -16,12 +16,16 @@
             </div>
         </div>
         <div style="height: 95%;display: flex;">
-            <div style="width: 10%;height: 100%;box-shadow: 0 1px 5px rgba(0,0,0,.2), 0 2px 2px rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12);">
-                <ep-menu :default-index="currentRoute" class="ep-menu-demo-vertical" theme="light">
-                    <ep-menu-item @click="switchRouter(value.path)" v-for="(value, key) in routes" :key="key" :index="value.path" icon="ios-home">{{value.meta.title}}</ep-menu-item>
+            <div style="width: 10%;height: 100%;box-shadow: 0 1px 5px rgba(0,0,0,.2), 0 2px 2px rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12);background-color: #001529;">
+                <ep-menu :default-index="currentRoute" class="ep-menu-demo-vertical" theme="dark">
+                    <ep-menu-item @click="switchRouter('/index')" index="/index" icon="ios-home">首页</ep-menu-item>
+                    <ep-submenu icon="person" title="信息维护">
+                        <ep-menu-item icon="information" @click="switchRouter('/baseUserInfo')" index="/baseUserInfo">基础信息</ep-menu-item>
+                        <ep-menu-item icon="information" @click="switchRouter('/securityUserInfo')" index="/securityUserInfo">安全中心</ep-menu-item>
+                    </ep-submenu>
                 </ep-menu>
             </div>
-            <div style="width: 90%;height: 100%;background-color: #00000014;padding: 20px;box-sizing: border-box;overflow: auto;">
+            <div style="width: 90%;height: 100%;background-color: #00000014;padding: 50px;box-sizing: border-box;overflow: auto;">
                 <router-view v-wechat-title="$route.meta.title"></router-view>
             </div>
         </div>
@@ -32,6 +36,7 @@
     import user from "../js/user.js";
     import pageConfig from "../config/pageConfig.js";
     import token from "../js/token.js";
+    import userManageApi from "../api/userManageApi.js";
 
     export default {
         name: "home",
@@ -53,6 +58,13 @@
                 token.delete();
                 user.delete();
                 window.location.href = pageConfig.index;
+            },
+
+            refreshUser() {
+                userManageApi.showUser().then((data) => {
+                    this.user = data;
+                    user.set(data);
+                });
             }
         },
 
