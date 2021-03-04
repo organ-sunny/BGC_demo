7d891fc4d9ea985835f21ba0136b715d24773ba2
yifulitesunny <1164806928@qq.com>
2021-03-04 17:24:15 +0800

    test github push

--- src/main/java/com/sunny/controller/ApiTestCaseController.java
+++ src/main/java/com/sunny/controller/ApiTestCaseController.java
@@ -31,6 +31,8 @@ public class ApiTestCaseController {
         return ResponseEntity.normalReturn("success", 200, null);
     }
 
+    // test
+
     @PostMapping("query")
     public ResponseEntity queryApiCase(@RequestBody(required = false) Map<String, Object> map) {
         // 执行查询
